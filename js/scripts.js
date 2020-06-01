18388808f15b243025967081f14519334fb1a2c2
knutab <knutab@users.noreply.github.com>
2020-06-01 21:37:10 +0200

    Created simple start of page

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        // - 70 is the offset/top margin
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 114
+        }, 800, function() {
+
+            // Add hash (#) to URL when done scrolling (default click behavior), without jumping to hash
+            if (history.pushState) {
+                history.pushState(null, null, hash);
+            } else {
+                window.location.hash = hash;
+            }
+        });
+        return false;
+    } // End if
+});
